64731d5cc180e0c698798634952ca07be4b30410
raisul1313 <raisul.islam1313@gmail.com>
2022-06-07 15:05:44 +0600

    touch up on tab colour

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -22,6 +22,7 @@ class Home extends StatelessWidget {
                   labelColor: Colors.red,
                   indicatorSize: TabBarIndicatorSize.label,
                   indicatorColor: Colors.red,
+                  unselectedLabelColor: Colors.grey,
                   tabs: [
                     Tab(
                       text: "Parsing Simple JSON",
